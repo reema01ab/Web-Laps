0d965bea8a45a9f70ec73b6ecc51c743564120ce
reema01ab <reemabandar9@gmail.com>
2026-04-14 21:43:31 +0300

    lab8

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('search', views.search_books, name='search_books'),
     path('simple/query',views.simple_query),
     path('complex/query', views.complex_query),
+    path('lab8/task1', views.lab8_task1, name='books.lab8.task1'),
+    path('lab8/task2', views.lab8_task2, name='books.lab8.task2'),
+    path('lab8/task3', views.lab8_task3, name='books.lab8.task3'),
+    path('lab8/task4', views.lab8_task4, name='books.lab8.task4'),
+    path('lab8/task5', views.lab8_task5, name='books.lab8.task5'),
+    path('lab8/task7', views.lab8_task7, name='books.lab8.task7'),
     ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-from .models import Book
 
-from django.shortcuts import render
+from django.db.models import Q, Count, Sum, Avg, Max, Min
 
+from .models import Book, Student
 #def index(request):
  #   name = request.GET.get("name", "")
  #   return render(request, "bookmodule/index.html", {"name": name})
@@ -78,3 +78,50 @@ def complex_query(request):
         return render(request, 'bookmodule/bookList.html', {'books': mybooks})
     else:
         return render(request, 'bookmodule/index.html')
+
+
+def lab8_task1(request):
+    books = Book.objects.filter(Q(price__lte=80))
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+
+def lab8_task2(request):
+    books = Book.objects.filter(
+        Q(edition__gt=3) &
+        (Q(title__icontains='qu') | Q(author__icontains='qu'))
+    )
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+
+def lab8_task3(request):
+    books = Book.objects.filter(
+        Q(edition__lte=3) &
+        (~Q(title__icontains='qu') | ~Q(author__icontains='qu'))
+    )
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+
+def lab8_task4(request):
+    books = Book.objects.all().order_by('title')
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+
+def lab8_task5(request):
+    stats = Book.objects.aggregate(
+        count_books=Count('id'),
+        total_price=Sum('price'),
+        average_price=Avg('price'),
+        max_price=Max('price'),
+        min_price=Min('price')
+    )
+    return render(request, 'bookmodule/bookStats.html', {'stats': stats})
+
+
+def lab8_task7(request):
+    city_counts = Student.objects.values('address__city').annotate(
+        student_count=Count('id')
+    ).order_by('address__city')
+
+    return render(request, 'bookmodule/student_city_count.html', {
+        'city_counts': city_counts
+    })
